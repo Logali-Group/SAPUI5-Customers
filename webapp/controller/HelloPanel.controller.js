c085b6e46e41075490e309ccf2c5235262d7274f
Logali-Group <training@logaligroup.com>
2020-04-30 12:24:29 +0000

    Fragment

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -1,7 +1,8 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/m/MessageToast",
+	"sap/ui/core/Fragment"
+], function (Controller, MessageToast, Fragment) {
 
 	return Controller.extend("logaligroup.Customers.controller.HelloPanel", {
 		
@@ -12,6 +13,25 @@ sap.ui.define([
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
 			// show message
 			MessageToast.show(sMsg);
+		},
+		
+		onOpenDialog : function () {
+			
+			var oView = this.getView();
+
+			if (!this.byId("helloDialog")) {
+				// load asynchronous XML fragment
+				Fragment.load({
+					id: oView.getId(),
+					name: "logaligroup.Customers.view.HelloDialog"
+				}).then(function (oDialog) {
+					// connect dialog to the root view of this component
+					oView.addDependent(oDialog);
+					oDialog.open();
+				});
+			} else {
+				this.byId("helloDialog").open();
+			}
 		}
 		
 	});
