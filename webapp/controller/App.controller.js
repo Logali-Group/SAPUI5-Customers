20701fa358fc9bc3c68b9a3895d77d1b160eae67
Logali-Group <training@logaligroup.com>
2020-04-30 11:28:33 +0000

    Interfaz de usuario

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,11 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller",
-	"sap/m/MessageToast"
-], function (Controller, MessageToast) {
+	"sap/ui/core/mvc/Controller"
+], function ( Controller ) {
 
 	return Controller.extend("logaligroup.Customers.controller.App", {
 
 		onInit: function () {
-		},
-
-		onShowHello: function () {
-			// read msg from i18n model
-			var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-			// show message
-			MessageToast.show(sMsg);
 		}
-	});
 
+	});
 });
