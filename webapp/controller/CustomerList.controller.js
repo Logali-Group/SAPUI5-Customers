a875ace2604fbb43b2272160eb2e3eaf970acfcd
Logali-Group <training@logaligroup.com>
2020-05-04 12:39:56 +0000

    filter

--- webapp/controller/CustomerList.controller.js
+++ webapp/controller/CustomerList.controller.js
@@ -1,8 +1,10 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
-	"../model/formatter"
-], function (Controller, JSONModel,formatter) {
+	"../model/formatter",
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator"
+], function (Controller, JSONModel,formatter, Filter, FilterOperator) {
 
 	return Controller.extend("logaligroup.Customers.controller.CustomerList", {
         
@@ -13,6 +15,21 @@ sap.ui.define([
 				currency: "USD"
 			});
 			this.getView().setModel(oViewModel, "view");
+		},
+		
+		onFilterCustomers : function (oEvent) {
+
+			// build filter array
+			var aFilter = [];
+			var sQuery = oEvent.getParameter("query");
+			if (sQuery) {
+				aFilter.push(new Filter("ContactName", FilterOperator.Contains, sQuery));
+			}
+
+			// filter binding
+			var oList = this.byId("customerList");
+			var oBinding = oList.getBinding("items");
+			oBinding.filter(aFilter);
 		}
 	});
 });
