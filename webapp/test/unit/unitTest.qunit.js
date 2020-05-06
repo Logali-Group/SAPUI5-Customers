09c19a5e92ae65a2ed12d19a366acd5018411fd8
Logali-Group <training@logaligroup.com>
2020-05-06 11:41:07 +0000

    Unidades de test

---
+++ webapp/test/unit/unitTest.qunit.js
@@ -0,0 +1,14 @@
+/* global QUnit */
+
+QUnit.config.autostart = false;
+
+sap.ui.getCore().attachInit(function () {
+
+	sap.ui.require([
+		"logaligroup/Customers/test/unit/model/formatter"
+	], function () {
+		
+		QUnit.start();
+		
+	});
+});
