53c887b17ef96c47c86077024c8ad333b9809834
Logali-Group <training@logaligroup.com>
2020-05-07 14:12:52 +0000

    Controler Personalizado

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/core/UIComponent",
-	"sap/ui/core/routing/History"
-], function (Controller, UIComponent, History) {
+	"sap/ui/core/routing/History",
+	"sap/m/MessageToast",
+], function (Controller, UIComponent, History, MessageToast) {
 	"use strict";
 
 	return Controller.extend("logaligroup.Customers.controller.Detail", {
@@ -18,6 +19,7 @@ sap.ui.define([
 		},
 
 		_onObjectMatched: function (oEvent) {
+			this.byId("rating").reset();
 			this.getView().bindElement({
 				path: "/" + window.decodeURIComponent(oEvent.getParameter("arguments").customerDetails),
 				model: "customerWz"
@@ -34,6 +36,13 @@ sap.ui.define([
 				var oRouter = UIComponent.getRouterFor(this);
 				oRouter.navTo("overview", {}, true);
 			}
+		},
+		
+		onRatingChange: function(oEvent) {
+			var fValue = oEvent.getParameter("value");
+			var oResourceBundle = this.getView().getModel("i18n").getResourceBundle();
+			
+			MessageToast.show(oResourceBundle.getText("ratingConfirmation", [fValue]));
 		}
 
 		/**
